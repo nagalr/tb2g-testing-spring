05dc3c6efaadf536be6eb7e3c4d68dd6f11ac45a
Ronnen <nagalr@gmail.com>
2021-03-04 17:59:51 +0200

    Converted HearingInterpreterYannyTest into an Integration-test by @Autowire, other relevant annotations and bring-in the context

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
@@ -1,7 +1,10 @@
 package org.springframework.samples.petclinic.sfg;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,15 +12,12 @@ import static org.junit.Assert.assertEquals;
  * Created by ronnen on 04-Mar-2021
  */
 
-
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {BaseConfig.class, YannyConfig.class})
 public class HearingInterpreterYannyTest {
 
+    @Autowired
     HearingInterpreter hearingInterpreter;
-
-    @Before
-    public void setUp() throws Exception {
-        hearingInterpreter = new HearingInterpreter(new YannyWordProducer());
-    }
 
     @Test
     public void whatIheard() {
