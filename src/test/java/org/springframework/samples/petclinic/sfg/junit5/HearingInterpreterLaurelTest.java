09147b81f5ad115535a4e02abc2fb461599790df
Ronnen <nagalr@gmail.com>
2021-03-04 18:16:42 +0200

    Convert the HearingInterpreterLaurelTest class to use JUnit5 syntax annotation '@SpringJUnitConfig'

--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterLaurelTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterLaurelTest.java
@@ -1,13 +1,21 @@
 package org.springframework.samples.petclinic.sfg.junit5;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.LaurelConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 /**
  * Created by ronnen on 04-Mar-2021
  */
 
-
+@SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})
 class HearingInterpreterLaurelTest {
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
 
     @Test
     void whatIheard() {
