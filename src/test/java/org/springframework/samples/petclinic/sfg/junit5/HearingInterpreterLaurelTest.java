8301d2d3f913ec4bc6beb919757f8c60b6aef798
Ronnen <nagalr@gmail.com>
2021-03-04 18:10:22 +0200

    Add HearingInterpreterLaurelTest test-class based on JUnit5

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterLaurelTest.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by ronnen on 04-Mar-2021
+ */
+
+
+class HearingInterpreterLaurelTest {
+
+    @Test
+    void whatIheard() {
+    }
+}
