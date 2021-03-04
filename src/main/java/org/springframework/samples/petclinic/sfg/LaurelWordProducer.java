d976c7864aac77fe4067198e309f63745ab153cb
Ronnen <nagalr@gmail.com>
2021-03-04 15:47:18 +0200

    chore: Created Yanny-Laurel classes, sfg package for further testing

---
+++ src/main/java/org/springframework/samples/petclinic/sfg/LaurelWordProducer.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic.sfg;
+
+/**
+ * Created by ronnen on 04-Mar-2021
+ */
+
+
+public class LaurelWordProducer implements WordProducer {
+    @Override
+    public String getWord() {
+        return "Laurel";
+    }
+}
---
+++ src/main/java/org/springframework/samples/petclinic/sfg/WordProducer.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.sfg;
+
+/**
+ * Created by ronnen on 04-Mar-2021
+ */
+
+
+public interface WordProducer {
+
+    String getWord();
+}
