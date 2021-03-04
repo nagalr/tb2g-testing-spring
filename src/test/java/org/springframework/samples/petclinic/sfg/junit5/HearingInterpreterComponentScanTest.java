a4f77e323e383637feb9480b66556d110bcb8d9e
Ronnen <nagalr@gmail.com>
2021-03-04 20:16:01 +0200

    Implement HearingInterpreterComponentScanTest, a test class with inner-configuration-class that has a path to @ComponentScan

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponentScanTest.java
@@ -0,0 +1,33 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by ronnen on 04-Mar-2021
+ */
+
+@SpringJUnitConfig(classes = HearingInterpreterComponentScanTest.TestConfig.class)
+class HearingInterpreterComponentScanTest {
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig {
+
+    }
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIheard() {
+        String word = hearingInterpreter.whatIheard();
+        assertEquals("Laurel", word );
+    }
+}
