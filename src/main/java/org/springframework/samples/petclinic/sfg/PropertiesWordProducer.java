5a8079fb326420c923c62f7f21c4bd2b6778f3a8
Ronnen <nagalr@gmail.com>
2021-03-07 14:26:14 +0200

    Add PropertiesWordProducer class with @Profile("externalized")

---
+++ src/main/java/org/springframework/samples/petclinic/sfg/PropertiesWordProducer.java
@@ -0,0 +1,29 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ronnen on 07-Mar-2021
+ */
+
+@Component
+@Primary
+@Profile("externalized")
+public class PropertiesWordProducer implements WordProducer {
+
+    private String word;
+
+    @Value("${say.word}")
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public String getWord() {
+        return word;
+    }
+}
+
