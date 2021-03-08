0bcf7fd6c4500f2fae5054ad08523daf2552b6a0
Ronnen <nagalr@gmail.com>
2021-03-08 19:10:21 +0200

    Add testFindByNameFound test method that include testing a redirect uri

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -2,33 +2,49 @@ package org.springframework.samples.petclinic.web;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.BindingResult;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // This test-class based on xml configurations.
 // the 'clinicService' Mock definition in 'mvc-test-config.xml'
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
+@ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
 
-    @Autowired
-    OwnerController ownerController;
-
-    @Autowired
+    @Mock
     ClinicService clinicService;
+
+    @Mock
+    BindingResult bindingResult;
+
+    @InjectMocks
+    OwnerController ownerController;
 
     MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
-    }
+            }
 
     @Test
     void testFindByNameNotFound() throws Exception {
@@ -36,6 +52,34 @@ class OwnerControllerTest {
                 .param("lastName", "James"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/findOwners"));
+    }
+
+    @Test
+    void testFindByNameFound() throws Exception {
+
+        Collection<Owner> owners = new HashSet<>();
+
+        Owner owner = new Owner();
+        owner.setAddress("");
+        owner.setCity("");
+        owner.setTelephone("");
+        owner.setFirstName("");
+        owner.setLastName("");
+        owner.setId(1);
+
+        owners.add(owner);
+
+        // given
+        given(clinicService.findOwnerByLastName(anyString())).willReturn(owners);
+
+        // when
+        ownerController.processFindForm(owner, bindingResult, new HashMap<>());
+
+        //then
+        then(clinicService).should(times(1)).findOwnerByLastName(anyString());
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(redirectedUrl("/owners/1"));
     }
 
     @Test
