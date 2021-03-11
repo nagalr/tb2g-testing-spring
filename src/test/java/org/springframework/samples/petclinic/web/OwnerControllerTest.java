887865224ee889c42e1dab04509e04d3c598fbce
Ronnen <nagalr@gmail.com>
2021-03-11 11:50:31 +0200

    Populate OwnerControllerTest.java with comments

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -41,7 +41,7 @@ class OwnerControllerTest {
     @Mock
     BindingResult bindingResult;
 
-    // its mock definition located in the xml file
+    // its mock definition located in the xml file 'mvc-test-config.xml'
     @Autowired
     ClinicService clinicService;
 
@@ -56,11 +56,23 @@ class OwnerControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
     }
 
+    /*
+     the same mock involved in many methods here
+     hence, without a 'reset' after every method-test
+     an error will arise when running the all-tests-class
+     while every test-method works fine.
+    */
     @AfterEach
     void tearDown() {
         reset(clinicService);
     }
 
+    /*
+     testing the 'get' route "/owners/new".
+     using only mockMvc here, no Mocks more than that.
+     checking the status isOK(), a new attribute added to model,
+     and the return view is as expected.
+     */
     @Test
     void initCreationFormTest() throws Exception {
         mockMvc.perform(get("/owners/new"))
@@ -70,8 +82,11 @@ class OwnerControllerTest {
     }
 
     /*
+     testing the 'post' route of "/owners/new".
+     checking only the first scenario of the method, entering the 'if' block.
      not including here Owner Object properties populated with values,
      hence, 'BindingResult' will have errors, entering the 'if' block.
+     checking status isOK(), and return view is as expected.
     */
     @Test
     void processCreationFormResultHasErrorsTest() throws Exception {
@@ -81,8 +96,11 @@ class OwnerControllerTest {
     }
 
     /*
+     testing the 'post' route of "/owners/new".
+     checking here the second scenario of the method, entering the 'else' block.
      Populating the Owner Object properties to hold the '@Valid' restrictions.
      'BindingResult' will have no errors, so the 'else' scenario will occur.
+     checking that a 'redirect' is performed to the right route.
      */
     @Test
     void processCreationFormResultNoErrorsTest() throws Exception {
@@ -97,6 +115,11 @@ class OwnerControllerTest {
                 .andExpect(redirectedUrl("/owners/1"));
     }
 
+    /*
+     testing the 'get' route of "/owners/find"
+     checking the status isOK(), an attribute added to the model,
+     and the right view returned.
+     */
     @Test
     void initFindFormTest() throws Exception {
         mockMvc.perform(get("/owners/find"))
@@ -105,6 +128,13 @@ class OwnerControllerTest {
                 .andExpect(view().name("owners/findOwners"));
     }
 
+    /*
+     the following three test methods test 'processFindForm', "/owners" route.
+     checking here the first scenario, 'results.isEmpty()' return true
+     since there is no definition to the 'clinicService' mock behaviour,
+     transferring one parameter to the get() call,
+     checking the status isOK(), and the right view returned.
+     */
     @Test
     void processFindFormNotFoundTest() throws Exception {
         mockMvc.perform(get("/owners")
@@ -113,6 +143,15 @@ class OwnerControllerTest {
                 .andExpect(view().name("owners/findOwners"));
     }
 
+    /*
+     testing here the second scenario of 'processFindForm' "/owners" route.
+     define a Collection of 'Owners' with one real Owner object within,
+     the Collection will be the returned value of 'clinicService' mock
+     on the method 'findOwnerByLastName' as we defined that in the 'given' code.
+     verifying the mock had one call on this method, and moving to mockMvc,
+     checking with mockMvc the redirect route is right, and includes the 'id'
+     of the Owner object that we defined. (value 1)
+     */
     @Test
     void processFindFormFoundOneTest() throws Exception {
         Collection<Owner> owners = new HashSet<>();
@@ -134,6 +173,16 @@ class OwnerControllerTest {
                 .andExpect(redirectedUrl("/owners/1"));
     }
 
+    /*
+     testing here the third scenario of 'processFindForm' "/owners" route.
+     here, a need to have more than one Owner object in the 'results' Collection,
+     hence, creating two Owners, and inserting them into the Collection.
+     the Collection will be the returned value of 'clinicService' mock
+     as defined in the 'given' code on the call to 'findOwnerByLastName'.
+     verify the 'clinicService' mock had one call on 'findOwnerByLastName',
+     that the status isOK(), an attribute added to the model,
+     and the return view is right.
+     */
     @Test
     void processFindFormFoundMultipleTest() throws Exception {
         Collection<Owner> owners = new HashSet<>();
@@ -158,6 +207,15 @@ class OwnerControllerTest {
                 .andExpect(view().name("owners/ownersList"));
     }
 
+    /*
+     testing the 'get' route "/owners/{ownerId}/edit".
+     define the 'clinicService' mock returned value on 'findOwnerById' call,
+     and verify this mock interaction.
+     mockMvc 'perform' here on this 'get' route, since we have a @PathVariable
+     we pass the value '1' as argument to 'perform', as a @PathVariable value.
+     checking the status isOK(), an attribute added,
+     and the right view returned.
+     */
     @Test
     void initUpdateOwnerFormTest() throws Exception {
         // given
@@ -175,6 +233,16 @@ class OwnerControllerTest {
                 .andExpect(view().name("owners/createOrUpdateOwnerForm"));
     }
 
+    /*
+     testing the first scenario of the 'post' route "/owners/{ownerId}/edit".
+     entering the 'if' block since result.hasErrors() is true, meaning,
+     the BindingResult object return true since there are no values to
+     the Owner object properties, which is under @Valid, and all of its properties
+     must include a value. (as they defined)
+     mockMvc 'perform' here on this 'post' route. since we have a @PathVariable
+     we pass the value '1' as argument to 'perform', as a @PathVariable value.
+     checking the status isOK(), and the right view returned.
+     */
     @Test
     void processUpdateOwnerFormHasErrorsTest() throws Exception {
         mockMvc.perform(post("/owners/{ownerId}/edit", 1))
@@ -182,6 +250,23 @@ class OwnerControllerTest {
                 .andExpect(view().name("owners/createOrUpdateOwnerForm"));
     }
 
+    /*
+     testing the second scenario of the 'post' route "/owners/{ownerId}/edit".
+     mockMvc 'perform' here on this 'post' route. since we have a @PathVariable
+     we pass the value '1' as argument to 'perform', as a @PathVariable value.
+     mockMvc pass parameters to populate the Owner object properties in the method.
+     all needed properties according to their definitions populated with values,
+     hence, the BindingResult object return 'false' on 'hasErrors()' call,
+     and entering the 'else' block. (the @Valid pass)
+     This is one of the reasons of using mockMvc, to see if the Object populated
+     properly inside a method, as expected.
+     NOTICE the 'param' passed to the 'id' property must defined as 'Id'
+     it's not working with 'id'. important!
+     passing Owner object properties-values include 'id' equals to 2,
+     to see that 'id' value indeed changes inside the method to the @PathVariable
+     value that defined to be 1. (in the line 'owner.setId(ownerId)' )
+     checking the right redirect view return. (includes the '1' value)
+     */
     @Test
     void processUpdateOwnerFormNoErrorsTest() throws Exception {
         mockMvc.perform(post("/owners/{ownerId}/edit", 1)
@@ -195,6 +280,14 @@ class OwnerControllerTest {
                 .andExpect(redirectedUrl("/owners/1"));
     }
 
+    /*
+     testing the route "/owners/{ownerId}". ('showOwner' method)
+     define here 'clinicService' (mock) on 'findOwnerById' call to return
+     a pre-defined Owner object. verify the mock interactions.
+     mockMvc 'perform' here on this route. since we have a @PathVariable
+     we pass the value '1' as argument to 'perform', as a @PathVariable value.
+     checking the status isOK().
+     */
     @Test
     void showOwnerTest() throws Exception {
         Owner owner = new Owner();
